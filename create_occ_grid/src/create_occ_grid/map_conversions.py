8299ba47074f9f0b13b8ff64e51ed00b5bc59a64
tus19188 <143554274+tus19188@users.noreply.github.com>
2024-02-20 16:21:09 -0500

    Update map_conversions.py
    Chris's Contributions

--- create_occ_grid/src/create_occ_grid/map_conversions.py
+++ create_occ_grid/src/create_occ_grid/map_conversions.py
@@ -1,7 +1,7 @@
 import math
 import numpy as np
 
-def sub2ind(array_shape, rows, cols):
+def sub2ind(array_shape, rows, cols): #PASSED!!!
     # sub2ind coverts subscript (row, column) pairs into linear indices in
     # row-major order
     #
@@ -16,10 +16,15 @@ def sub2ind(array_shape, rows, cols):
     #
     
     ##### YOUR CODE STARTS HERE #####
-    pass
+    ind = rows * array_shape[1] + cols
+    inv_ind = (rows < 0) | (rows >= array_shape[0]) | (cols < 0) | (cols >= array_shape[1])
+    for i in range(len(ind)):
+        if inv_ind[i]:
+            ind[i] = -1
+    return ind #outputs = numpy array of integer indices
     ##### YOUR CODE ENDS HERE   #####
 
-def ind2sub(array_shape, ind):
+def ind2sub(array_shape, ind):  #PASSED!!!
     # ind2sub converts linear indices in a row-major array to subscript
     # (row, column) pairs
     #
@@ -34,7 +39,14 @@ def ind2sub(array_shape, ind):
     #
     
     ##### YOUR CODE STARTS HERE #####
-    pass
+    rows = ind // array_shape[1]
+    cols = ind % array_shape[1]
+    inv_ind = (ind < 0) | (rows >= array_shape[0]) | (cols < 0) | (cols >= array_shape[1])
+    for i in range(len(ind)):
+        if inv_ind[i]:
+            rows[i] = -1
+            cols[i] = -1
+    return rows, cols
     ##### YOUR CODE ENDS HERE   #####
     
 def xy2sub(boundary, res, x, y):
@@ -53,10 +65,29 @@ def xy2sub(boundary, res, x, y):
     #
     
     ##### YOUR CODE STARTS HERE #####
-    pass
+    rows = np.floor((y-boundary[1]) / res).astype(int)
+    cols = np.floor((x-boundary[0]) / res).astype(int)
+    
+    #for points on top right of boundary
+    rows[y == boundary[3]] = int((boundary[3] - boundary[1]) / res) - 1
+    cols[x == boundary[2]] = int((boundary[2] - boundary[0]) / res) - 1
+
+
+    #for np.nan points
+    rows[np.isnan(x) | np.isnan(y)] = -1
+    cols[np.isnan(x) | np.isnan(y)] = -1
+
+    #for all other points
+    inv_ind = (x < boundary[0]) | (x > boundary[2]) | (y < boundary[1]) | (y > boundary[3])
+    for i in range(len(x)):
+        if inv_ind[i]:
+            rows[i] = -1
+            cols[i] = -1
+  
+    return rows, cols
     ##### YOUR CODE ENDS HERE   #####
     
-def sub2xy(boundary, res, rows, cols):
+def sub2xy(boundary, res, rows, cols): #PASSED!!!
     # sub2xy converts (row, column) subscript pairs into (x,y) coordinate pairs
     #
     # inputs:
@@ -72,7 +103,15 @@ def sub2xy(boundary, res, rows, cols):
     #
     
     ##### YOUR CODE STARTS HERE #####
-    pass
+    x = boundary[0] + res * (cols + 0.5)
+    y = boundary[1] + res * (rows +0.5)
+    inv_ind = (x < boundary[0]) | (x > boundary[2]) | (y < boundary[1]) | (y > boundary[3])
+    for i in range(len(x)):
+        if inv_ind[i]:
+            x[i] = np.NaN
+            y[i] = np.NaN
+    return x, y
+    
     ##### YOUR CODE ENDS HERE   #####
 
 def xy2ind(boundary, res, array_shape, x, y):
@@ -109,6 +148,6 @@ def ind2xy(boundary, res, array_shape, ind):
     #
     
     rows, cols = ind2sub(array_shape, ind)
-    x, y = sub2xy(boundary, res, rows, col)
+    x, y = sub2xy(boundary, res, rows, cols)
     return (x, y)
 
